f9c364e99d1226711ca11debf8e746ee0827a5c3
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2025-01-28 11:21:33 -0300

    feat: simplify posts listing in the homepage

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -5,13 +5,13 @@ type Props = {
   posts: Post[];
 };
 
-export function MoreStories({ posts }: Props) {
+export function PostsList({ posts }: Props) {
   return (
     <section>
       <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
+        Posts
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="flex flex-col gap-4">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
